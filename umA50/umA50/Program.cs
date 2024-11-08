7e85790f43d365dc7a497c636e974b22ad8eaefb
Lidia-gif <lidiamaria.202069@gmail.com>
2024-11-08 13:01:40 +0000

    Add files via upload

---
+++ umA50/umA50/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    //Exercício 2
+public class umA50
+{
+    public static void Main(string[] args)
+    {   //Somar os números ímpares 1 a 50
+        int soma = 0;
+        for (int contador = 1; contador <= 50; contador++)
+        {
+            if (contador % 2 != 0)
+            {
+                soma = soma + contador;
+            }
+        }
+            Console.WriteLine("O total da soma dos números ímpares é igual a: " + soma);
+    }
+}
